4fd0f9a03568cd6fddacd2f96f69a2a27f7a3a77
Vitor <vitorcorrea11@hotmail.com>
2019-06-12 18:37:09 -0300

    add new font and many other improvements on css and images

--- moderno-HTML-CSS-Vue/src/App.vue
+++ moderno-HTML-CSS-Vue/src/App.vue
@@ -11,7 +11,63 @@
 </script>
 
 <style>
+	@font-face {
+		font-family: 'Maipo Sans SemiBold';
+		src: url('assets/MaipoSansSemiBold.otf');
+	}
 	#app {
-		
+		font-family: 'Maipo Sans SemiBold';
+		color: #595252;
+	}
+	p {
+		font-size: 0.8em;
+	}
+	.colorWhite {
+		color: white;
+	}
+	.colorGrey {
+		color: #938b8b;
+	}
+	.mt {
+		margin-top: 3rem;
+	}
+	.mb {
+		margin-bottom: 4rem;
+	}
+	.mr {
+		margin-right: 1rem;
+	}
+	.ml {
+		margin-left: 1rem;
+	}
+	.smMt {
+		margin-top: 1rem;
+	}
+	.smMb {
+		margin-bottom: 1rem;
+	}
+	a {
+		display: inline-block;
+		width: 20rem;
+		height: 2rem;
+		padding-top: 0.5rem;
+		background-color: #393939;
+		color: #938b8b;
+		text-decoration: none;
+	}
+	.imagemBg {
+		background-color: #A9BACE;
+		background-blend-mode: multiply;
+		background-size: 100% 100%;
+        background-repeat: no-repeat;
+	}
+	.flexCenter {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+	.mainWidth {
+		width: 40vw;
 	}
 </style>
--- moderno-HTML-CSS-Vue/src/components/home.vue
+++ moderno-HTML-CSS-Vue/src/components/home.vue
@@ -1,74 +1,81 @@
 <template>
     <div class="home">
-        <div class="designLab">
-            <h1>WORKING WITH DESIGN LAB</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-            </p>
-            <div>
-                <h2>WHAT WE DO</h2>
-                <div>
+        <div class="designLab colorWhite imagemBg flexCenter">
+            <div class="mainWidth">
+                <i class="fas fa-jedi fa-2x"></i>
+                <h1 class="mt">WORKING <br> WITH DESIGN LAB</h1>
+                <p class="smMt">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            </div>
+        </div>
+        <div class="whatWeDo flexCenter mt mb">
+            <div class="mainWidth">
+                <h2 class="mb">WHAT WE DO</h2>
+                <div class="mt mb">
+                    <i class="fas fa-pen-nib fa-2x"></i>
                     <h3>DESIGN</h3>
-                    <p>
+                    <p class="smMt colorGrey">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        consequat.
                     </p>
                 </div>
-                <div>
+                <div class="mt mb">
+                    <i class="fas fa-laptop-code fa-2x"></i>
                     <h3>DEVELOPMENT</h3>
-                    <p>
+                    <p class="smMt colorGrey">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        cillum dolore eu fugiat nulla pariatur.
                     </p>
                 </div>
-                <div>
+                <div class="mt mb">
+                    <i class="far fa-calendar-check fa-2x"></i>
                     <h3>PRODUCT MANAGEMENT</h3>
-                    <p>
+                    <p class="smMt colorGrey">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        consequat.
                     </p>
                 </div>
             </div>
-            <div>
+        </div>
+        <div class="services colorWhite imagemBg flexCenter">
+            <div class="mainWidth">
                 <h2>SERVICES</h2>
-                <p>
+                <p class="mt">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    consequat. <br> Duis aute irure dolor in reprehenderit in voluptate velit esse
                     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
-            <div>
+        </div>
+        <div class="contactUs flexCenter mt mb">
+            <div class="mainWidth">            
                 <h2>CONTACT US</h2>
-                <p>
+                <p class="mt colorGrey">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    consequat.
                 </p>
-                <button>
-                    <router-link to='contact'>Contato</router-link>
-                </button>
+                <router-link to='contact' class="smMt smMb">CONTACT US</router-link>
             </div>
+        </div>
+        <div class="tradeMark flexCenter">
+            <i class="fas fa-jedi mr"></i>
+            <p class="colorGrey">&copy 2019, All Rights Reserved </p>
         </div>
     </div>
 </template>
@@ -86,6 +93,19 @@
 <style scoped>
     .home {
         text-align: center;
+    }
+    .designLab {
+        height: 97vh;
         background-image: url("../assets/bg1.jpg");
+    }
+    .fa-pen-nib {
+        transform: rotate(135deg);
+    }
+    .services {
+        height: 70vh;
+        background-image: url("../assets/bg2.jpg");
+    }
+    .tradeMark {
+        flex-direction: row;
     }
 </style>
